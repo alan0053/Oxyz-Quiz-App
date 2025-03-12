e2203d1d99ddf541d2a3ad7352fe52c92db21cc3
Berk <alan0053@algonquinlive.com>
2025-03-11 23:58:02 -0400

    Initial Commit

---
+++ Oxyz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Oxyz
+//
+//  Created by Berkcan Alankoy on 2025-03-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Oxyz/OxyzApp.swift
@@ -0,0 +1,17 @@
+//
+//  OxyzApp.swift
+//  Oxyz
+//
+//  Created by Berkcan Alankoy on 2025-03-11.
+//
+
+import SwiftUI
+
+@main
+struct OxyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
